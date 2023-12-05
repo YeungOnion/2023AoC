36f520f364f223b540b77daff589456638567e8f
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2023-12-04 23:07:53 -0600

    more progress, much more tests

--- 03/03.go
+++ 03/03.go
@@ -1,34 +1,99 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	return
+	filename := "03/input.txt"
+
+	// stream file by words
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	// read first two lines
+	lines := make([]string, 3, 3)
+	_, lines[1] = fileScanner.Scan(), fileScanner.Text()
+	_, lines[2] = fileScanner.Scan(), fileScanner.Text()
+	lines[0] = strings.Repeat(".", len(lines[1]))
+
+	score := ScoreMiddleLine(lines)
+
+	for fileScanner.Scan() {
+		lines[0] = lines[1]
+		lines[1] = lines[2]
+		lines[2] = fileScanner.Text()
+
+		score += ScoreMiddleLine(lines)
+	}
+	lines[0] = lines[1]
+	lines[1] = lines[2]
+	lines[2] = strings.Repeat(".", len(lines[1]))
+
+	fmt.Println("score: ", score)
 }
 
-func GetNumbers(numberRow string) [][]int {
+func ScoreMiddleLine(lines []string) int {
+	score := 0
+	for _, index := range GetNumberIndexes(lines[1]) {
+		number, err := strconv.Atoi(lines[1][index[0]:index[1]])
+		if err != nil {
+			panic("cannot convert digit sequence to number")
+		}
+		score += number
+	}
+	return score
+}
+
+func HasNeighborSymbol(index []int, neighborLines []string) bool {
+	return Any(neighborLines, func(line string) bool {
+		return AdjacentColumn(index, line)
+	})
+}
+
+func GetNumberIndexes(numberRow string) [][]int {
 	digitRe := regexp.MustCompile(`\d+`)
 	return digitRe.FindAllStringIndex(string(numberRow), -1)
-
 }
 
 // AdjacentColumn checks if the provided string has a symbol in a column adjcent to the location of an unspecified match string
 // uses the regexp convention of the match string, s[matchIndex[0]:matchIndex[1]]
 func AdjacentColumn(matchIndex []int, searchRow string) bool {
-
 	symbolRe := regexp.MustCompile(`[^\d\.]`)
 	for _, index := range symbolRe.FindAllStringIndex(searchRow, -1) {
-		if matchIndex[0] < index[1] && index[1] < matchIndex[1] {
-			fmt.Println("found near front")
-			return true
-		}
-		if index[0] < matchIndex[1] {
+		if matchIndex[0] <= index[1] &&
+			index[0] <= matchIndex[1] {
 			return true
 		}
 	}
+	return false
+}
 
+func All[T ~[]V, V any](t T, pred func(V) bool) bool {
+	for _, v := range t {
+		if !pred(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func Any[T ~[]V, V any](t T, pred func(V) bool) bool {
+	for _, v := range t {
+		b := pred(v)
+		if b {
+			return true
+		}
+	}
 	return false
 }
--- 03/03_test.go
+++ 03/03_test.go
@@ -1,31 +1,59 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestAdjacentColumn(t *testing.T) {
 	tests := []struct {
 		in  string
-		cmp string
 		pos []int
+		cmp string
 		exp bool
 	}{
 		{
+			cmp: ".......", // [][]int {{0,1},{6,7}}
 			in:  "..123*.",
-			cmp: "..123*.",
-			pos: []int{2, 5},
-			exp: true,
-		},
-		{
-			in:  "..123*.",
-			cmp: "*....?.",
-			pos: []int{2, 5},
-			exp: true,
-		},
-		{
-			in:  "..123*.",
-			cmp: "*.....?", // [][]int {{0,1},{6,7}}
 			pos: []int{2, 5},
 			exp: false,
+		},
+		{
+			cmp: "..123*.", // [][]int{{5,6}}
+			in:  "..123*.",
+			pos: []int{2, 5},
+			exp: true,
+		},
+		{
+			cmp: "*....?.", // [][]int {{0,1},{6,7}}
+			in:  "..123*.",
+			pos: []int{2, 5},
+			exp: true,
+		},
+		{
+			cmp: "*....?.", // [][]int{{0,1},{5,6}}
+			in:  "..123*.",
+			pos: []int{2, 5},
+			exp: true,
+		},
+		{
+			cmp: "......?", // [][]int {{0,1},{6,7}}
+			in:  "..123*.",
+			pos: []int{2, 5},
+			exp: false,
+		},
+		{
+			cmp: "?......", // [][]int {{0,1},{6,7}}
+			in:  "..123*.",
+			pos: []int{2, 5},
+			exp: false,
+		},
+		{
+			cmp: ".?.....", // [][]int {{0,1},{6,7}}
+			in:  "..123*.",
+			pos: []int{2, 5},
+			exp: true,
 		},
 	}
 
@@ -35,6 +63,56 @@ func TestAdjacentColumn(t *testing.T) {
 		if got != test.exp {
 			t.Fatalf("\n%s\n%s\n", test.in, test.cmp)
 			t.Fatalf("got %v, expected %v\n", got, test.exp)
+		}
+	}
+}
+
+func TestGivenMiddleRow(t *testing.T) {
+	tests := []struct {
+		in       []string
+		expected bool
+	}{
+		{
+			in: []string{
+				".......",
+				"..123*.",
+				"*....?.",
+			},
+			expected: true,
+		},
+		{
+			in: []string{
+				".......",
+				"..123..",
+				"*....?.",
+			},
+			expected: true,
+		},
+		{
+			in: []string{
+				".......",
+				"..123..",
+				"*.....?",
+			},
+			expected: false,
+		},
+		{
+			in: []string{
+				".......",
+				"..123*.",
+				".......",
+			},
+			expected: true,
+		},
+	}
+
+	for _, test := range tests {
+		// assert number of matches since testing
+		index := GetNumberIndexes(test.in[1])[0]
+		if HasNeighborSymbol(index, test.in) != test.expected {
+			out := fmt.Sprintf("expected match? = %v, assertion failed with input\n", test.expected)
+			out += strings.Join(test.in, "\n")
+			t.Fatal(out)
 		}
 	}
 

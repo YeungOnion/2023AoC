4f6564e99b72f1dfb7ec8cfcfe8e2de3d0cfac16
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2023-12-06 16:19:56 -0600

    still working on 5, but something is up with the .git disappearing

--- 05/05.go
+++ 05/05.go
@@ -5,16 +5,22 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/samber/lo"
 )
 
-type seedRange struct {
-	src  int
-	dest int
+type seedMap struct {
+	src    int
+	dest   int
+	window int
 }
 
-type AocMap []seedRange
+type seedRange [2]seedMap
+
+type seedTable []seedMap
 
 func main() {
 	filename := "05/sample-a.txt"
@@ -29,14 +35,31 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	seedNums := ScanSeedLine(fileScanner)
-	fmt.Printf("seeds numbers\n\t>> %v", seedNums)
-	fileScanner.Scan() // consume empty newline
 
+	nextNums := make([]int, len(seedNums))
 	for fileScanner.Scan() { // This scan "eats" the map header
+		fmt.Printf("\nnow: %v\n", seedNums)
+		fmt.Println(fileScanner.Text())
+		lines := ScanWhile(fileScanner, NumberSequenceBuffered)
+		fmt.Println("\t" + strings.Join(lines, "\n\t"))
+		table := ParseRowsToTable(lines)
 
+		for i, n := range seedNums {
+			nextNums[i] = table.Eval(n)
+		}
+		seedNums = nextNums
 	}
 
+	fmt.Printf("now: %v\n\n", seedNums)
 	return
+}
+
+func MustAtoi(s string, _ int) int {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return val
 }
 
 func ScanSeedLine(fs *bufio.Scanner) []int {
@@ -69,7 +92,7 @@ func parseMap(s string) {
 	panic("unimplemented")
 }
 
-func PassthruMap(i int, m AocMap) int {
+func PassthruMap(i int, m seedTable) int {
 	panic("unimplemented")
 }
 
@@ -81,17 +104,80 @@ func NumberSequenceBuffered(fs *bufio.Scanner) bool {
 // ScanWhile consumes input from scanner via Scan, if Scanned matches the predicate
 // Then the text it appended to a string slice output
 func ScanWhile(fs *bufio.Scanner, pred func(*bufio.Scanner) bool) []string {
-	out := make([]string, 0, 16)
+	textRows := make([]string, 0, 16)
 	for fs.Scan() {
 		text := fs.Text()
 		if !pred(fs) {
 			break
 		} else {
-			out = append(out, text)
+			textRows = append(textRows, text)
 		}
 
 	}
-	return out
+
+	return textRows
 }
 
-func PushKeyThroughMap(key int, m []int)
+func ParseRowsToTable(rows []string) seedTable {
+	digitsRe := regexp.MustCompile(`\d+`)
+	s := seedTable(lo.Map(rows, func(item string, index int) seedMap {
+		nums := lo.Map(digitsRe.FindAllString(item, -1), MustAtoi)
+		return seedMap{src: nums[1], dest: nums[0], window: nums[2]}
+	}))
+
+	sort.Sort(s)
+
+	return s
+}
+
+// FloorSearch returns the element position that has the largest value of
+// seedMap.src not less than the needle
+func (c seedTable) FloorSearch(needle int) int {
+
+	if len(c) == 0 || needle < c[0].src {
+		// precedes the first (implies all) bin edges
+		// alternatively could have all seedTable's have seedMap{0,0,0}
+		return -1
+	}
+
+	var mid int
+	lb, ub := 0, len(c)
+	for lb < ub {
+		mid = (lb + ub) / 2
+		f := c[mid].src
+		if needle == f {
+			return mid
+		} else if needle < f {
+			ub = mid
+		} else {
+			lb = mid + 1
+		}
+	}
+
+	return mid
+
+}
+
+func (c seedTable) Eval(key int) int {
+	index := c.FloorSearch(key)
+	if index == -1 {
+		return key
+	}
+	elem := c[index]
+	if elem.src+elem.window <= key {
+		return key
+	}
+	return elem.dest + key - elem.src
+}
+
+func (c seedTable) Len() int {
+	return len(c)
+}
+
+func (c seedTable) Less(i, j int) bool {
+	return c[i].src < c[j].src
+}
+
+func (c seedTable) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
--- 05/05_test.go
+++ 05/05_test.go
@@ -7,7 +7,50 @@ import (
 	"testing"
 )
 
-func TestMap(t *testing.T) {
+func TestSearch(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		value    int
+		expected int
+	}{
+		{
+			value:    79,
+			expected: 81,
+		},
+		{
+			value:    98,
+			expected: 50,
+		},
+		{
+			value:    99,
+			expected: 51,
+		},
+		{
+			value:    100,
+			expected: 100,
+		},
+		{
+			value:    10,
+			expected: 10,
+		},
+	}
+
+	// construct sorted table
+	input := seedTable{
+		{src: 50, dest: 52, window: 48},
+		{src: 98, dest: 50, window: 2},
+	}
+
+	for _, test := range tests {
+		t.Logf("testing Eval(%d)\n", test.value)
+
+		got := input.Eval(test.value)
+		if got != test.expected {
+			t.Logf("failed test %s,\nEval(%d)\n%v", test.name, test.value, input)
+			t.Fatalf("got %d, expected %d", got, test.expected)
+		}
+	}
 	return
 }
 

41190f16c8e4116c2558f816ecf87a1e6f3a8500
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2023-12-07 00:52:23 -0600

    still working on 07, sample passed, test did not

--- 07/07.go
+++ 07/07.go
@@ -6,9 +6,22 @@ import (
 	"os"
 	"sort"
 	"strconv"
+
+	"github.com/samber/lo"
 )
 
-var forwardDigitRe, reverseDigitRe string
+type HandType int
+type Hand string
+
+const (
+	HighCard = iota
+	OnePair
+	TwoPair
+	ThreeKind
+	FullHouse
+	FourKind
+	FiveKind
+)
 
 func main() {
 	filename := os.Args[1]
@@ -26,50 +39,69 @@ func main() {
 	fileScanner.Split(bufio.ScanWords)
 
 	hands, bids := ParseInput(fileScanner)
-	N := len(hands)
-
-	fmt.Println(hands)
-	fmt.Println(bids)
 
 	ranks := RankHands(hands)
+	fmt.Println(bids)
+	fmt.Println(ranks)
 
 	winnings := 0
-	for i, position := range ranks {
-		winnings += (N - i) * bids[position]
+	for i, rank := range ranks {
+		winnings += rank * bids[i]
 	}
 
 	fmt.Println("winnings: ", winnings)
-
-	return
 }
 
-type HandType int
-
-type Hand string
-
-const (
-	HighCard = iota
-	OnePair
-	TwoPair
-	ThreeKind
-	FullHouse
-	FourKind
-	FiveKind
-)
-
 func Less(a, b Hand) bool {
-	aType, bType := EvaluateHandType(a), EvaluateHandType(b)
 
+	aType, bType := EvaluateHandType(a), EvaluateHandType(b)
 	if aType != bType {
 		return aType < bType
 	} else {
-		return a < b
+		return LessHandString(a, b)
 	}
+
+}
+
+func LessHandString(a, b Hand) bool {
+	// true if a-b < 0
+	aB, bB := []rune(a), []rune(b)
+	cB := make([]int, len(a))
+	for i := range aB {
+		cB[i] = int(aB[i] - bB[i])
+	}
+	return !Any(cB, func(i int) bool { return i < 0 })
 }
 
 func EvaluateHandType(h Hand) HandType {
+	runes := []rune(h)
+	dict := make(map[rune]int)
+	for _, symbol := range runes {
+		dict[symbol]++
+	}
 
-	panic("unimplemented")
+	switch len(dict) {
+	case 5:
+		return HighCard
+	case 4:
+		return OnePair
+	case 3:
+		if len(lo.PickByValues(dict, []int{3})) > 0 {
+			return ThreeKind
+		} else {
+			return TwoPair
+		}
+	case 2:
+		if len(lo.PickByValues(dict, []int{4})) > 0 {
+			return FourKind
+		} else {
+			return FullHouse
+		}
+	case 1:
+		return FiveKind
+	default:
+		panic("unreachable")
+	}
 }
 
 func ParseInput(fs *bufio.Scanner) ([]Hand, []int) {
@@ -86,13 +118,13 @@ func ParseInput(fs *bufio.Scanner) ([]Hand, []int) {
 }
 
 // RankHands returns n element slice from (0,n) such that
-// hands[RankHands[i-1]] is the ith largest (i=0 -> largest)
 func RankHands(hands []Hand) []int {
 	indexedHands := NewIndexPairing(hands)
 	sort.Sort(indexedHands)
+	// N := len(hands)
 	result := make([]int, len(hands))
 	for i, v := range indexedHands {
-		result[v.initPos] = i
+		result[i] = v.initPos + 1
 	}
 	return result
 }
@@ -123,4 +155,25 @@ func (s indexPairSlice) Less(i, j int) bool {
 
 func (s indexPairSlice) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
+}
+
+// Any is a short circuit at first true of Reduce(iterable ~[]V, predicate func(V) bool) bool
+func Any[T ~[]V, V any](t T, pred func(V) bool) bool {
+	for _, v := range t {
+		b := pred(v)
+		if b {
+			return true
+		}
+	}
+	return false
+}
+
+// All is a short circuit at first false of Reduce(iterable ~[]V, predicate func(V) bool) bool
+func All[T ~[]V, V any](t T, pred func(V) bool) bool {
+	for _, v := range t {
+		if !pred(v) {
+			return false
+		}
+	}
+	return true
 }
--- 07/07_test.go
+++ 07/07_test.go
@@ -1,5 +1,20 @@
 package main
 
-func TestIndexing() {
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIndexing(t *testing.T) {
+	hands := []Hand{
+		"12345", // 2
+		"01234", // 1
+		"00000", // 0
+	}
+
+	ranks := RankHands(hands)
+	fmt.Println(hands[ranks[0]])
+	fmt.Println(hands[ranks[1]])
+	fmt.Println(hands[ranks[2]])
 	return
 }
